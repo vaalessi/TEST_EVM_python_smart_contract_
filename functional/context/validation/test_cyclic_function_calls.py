a389541899ad0d4afc27db4ea78e944ce787285b
VinFTW <31747311+vaalessi@users.noreply.github.com>
2021-05-19 12:15:42 -0400

    Add files via upload

---
+++ functional/context/validation/test_cyclic_function_calls.py
@@ -0,0 +1,45 @@
+import pytest
+
+from vyper.ast import parse_to_ast
+from vyper.context.validation.module import ModuleNodeVisitor
+from vyper.exceptions import CallViolation
+
+
+def test_cyclic_function_call(namespace):
+    code = """
+@internal
+def foo():
+    self.bar()
+
+@internal
+def bar():
+    self.foo()
+    """
+    vyper_module = parse_to_ast(code)
+    with namespace.enter_scope():
+        with pytest.raises(CallViolation):
+            ModuleNodeVisitor(vyper_module, {}, namespace)
+
+
+def test_multi_cyclic_function_call(namespace):
+    code = """
+@internal
+def foo():
+    self.bar()
+
+@internal
+def bar():
+    self.baz()
+
+@internal
+def baz():
+    self.potato()
+
+@internal
+def potato():
+    self.foo()
+    """
+    vyper_module = parse_to_ast(code)
+    with namespace.enter_scope():
+        with pytest.raises(CallViolation):
+            ModuleNodeVisitor(vyper_module, {}, namespace)
